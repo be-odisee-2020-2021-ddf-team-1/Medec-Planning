41d15f09d729ea4b0590b70b339f2ad682626985
adrientaneko <55691233+adrientaneko@users.noreply.github.com>
2021-05-11 19:17:09 +0200

    update PlanningController

--- src/main/java/com/example/demo/domain/Planning.java
+++ src/main/java/com/example/demo/domain/Planning.java
@@ -11,17 +11,17 @@ import java.util.List;
 @Table(name ="PLANNING")
 @Data
 @RequiredArgsConstructor
-@NoArgsConstructor(force=true)
+//@NoArgsConstructor(force=true)
 public class Planning implements Serializable {
 
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
 
-    private final long planningId;
+    private long planningId = 0;
 
     private  String name;
     @XmlElement(name="name")
-    public String getNaam() {
+    public String getName() {
         return name;
     }
 
--- src/main/java/com/example/demo/services/PlanningService.java
+++ src/main/java/com/example/demo/services/PlanningService.java
@@ -3,20 +3,22 @@ package com.example.demo.services;
 
 import com.example.demo.domain.Planning;
 
+import java.util.Optional;
+
 public interface PlanningService {
 
 
         void CreatePlanning(Planning planning);
+        void UpdatePlanning(Planning planning);
 
- /*   Planning getPlanningById(long planningId);
+   Optional<Planning> getPlanningById(long planningId);
 
     Planning getPlanningByNaam(String planningNaam);
 
     void deletePlanning(long planningId);
 
-    List<Planning> getPlannings();
+    //List<Planning> getPlannings();
 
-    List<Medewerker> getMedewerkers();*/
     }
 
 
